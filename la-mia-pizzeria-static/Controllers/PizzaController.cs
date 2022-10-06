6645f82742de9dc088704c9dd3ddc5d4e4b6bd57
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-06 14:55:48 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,107 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient.Server;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers
+{
+    public class PizzaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                List<Pizza> pizzas = context.Pizzas.ToList<Pizza>();
+                return View("Index", pizzas);
+            }
+        }
+
+        public IActionResult Detail(int id)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza pizzaDetail = context.Pizzas.Where(pizza => pizza.PizzaID == id).FirstOrDefault();
+
+                if ( pizzaDetail == null)
+                {
+                    return NotFound("La pizza con id non è stata trovata");
+                } else
+                {
+                    return View("Detail", pizzaDetail);
+                }
+                
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create (Pizza formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", formData);
+            }
+
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza pizza = new Pizza();
+                pizza.Name = formData.Name;
+                pizza.Description = formData.Description;
+                pizza.Photo = formData.Photo;
+                pizza.Price = formData.Price;
+
+                context.Pizzas.Add(pizza);
+
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Update (int id)
+        {
+            PizzaContext context = new PizzaContext();
+
+            Pizza pizza = context.Pizzas.Where(pizza => pizza.PizzaID == id).FirstOrDefault();
+            
+            if (pizza == null)
+            {
+                return NotFound("Pizza non trovata");
+            }
+
+            return View(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, Pizza formData)
+        {
+            PizzaContext context = new PizzaContext();
+
+            Pizza pizza = context.Pizzas.Where(pizza => pizza.PizzaID == id).FirstOrDefault();
+
+            if(pizza != null)
+            {
+                pizza.Name = formData.Name;
+                pizza.Description = formData.Description;
+                pizza.Photo = formData.Photo;
+                pizza.Price = formData.Price;
+            }else
+            {
+                return NotFound("Pizza non trovata");
+            }
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ la-mia-pizzeria-static/Program.cs
@@ -0,0 +1,40 @@
+using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+
+app.Run();
+
+public class PizzaContext : DbContext
+{
+    public DbSet<Pizza> Pizzas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=pizzeria-db;Integrated Security=True");
+    }
+}
