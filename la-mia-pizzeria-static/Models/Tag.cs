fa9b02cbcf4993ec2f25a636568aaa783833dcd6
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-10 14:44:15 +0200

    creata classe tag

---
+++ la-mia-pizzeria-static/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace la_mia_pizzeria_crud_mvc.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+        public Tag()
+        {
+
+        }
+    }
+}
